e96a02eb33afed588097e1c1769ec311d1a74424
wangjiwen7 <63782184+wangjiwen7@users.noreply.github.com>
2020-08-17 21:33:39 +0800

    Update M estimates from P-spline-smoothed curve.R

--- M estimates from P-spline-smoothed curve.R
+++ M estimates from P-spline-smoothed curve.R
@@ -1,9 +1,10 @@
 ## Project: Mortality in G7 countries
 # G7: United States (US), Canada, France, Germany, Italy, Japan, and the United Kingdom (UK)
+## An R routine for computing M estimates from P-spline-smoothed mortality curves
 ## Author: Karen Cheung, Jiwen Wang
 ## Updated on 2020 Agust 17th
 
-## An R routine for computing M estimates from P-spline-smoothed mortality curves
+
 
 ##To smooth death counts with P-splines over a given ranges of ages, the "Mort1Dsmooth" function of the MortalitySmooth package in R (Camarda 2012) is used.
 
@@ -11,8 +12,8 @@ library("MortalitySmooth")
 fit <- "Mort1Dsmooth"(x = age, y = D, offset = log(E))
 
 ##where age is a vector with the age values for smoothing (age>=10 in our case), and the vectors D and E denotes the observed death counts and person-years lived in each age interval. 
-#The output of the“Mort1Dsmooth” function is a fit object that contains information about the fitting of the Poisson regression model and about the original data.
-#In order to retrieve our estimation process, and to compute standard errors and hence 95% confidence interval for response and log-rates:
+ #The output of the“Mort1Dsmooth” function is a fit object that contains information about the fitting of the Poisson regression model and about the original data.
+ #In order to retrieve our estimation process, and to compute standard errors and hence 95% confidence interval for response and log-rates:
 
 summary(fit)
 fit1Dfor <- predict(fit, newdata = x,se.fit = TRUE)
@@ -20,7 +21,7 @@ fit1Dfor <- predict(fit, newdata = x,se.fit = TRUE)
 se <- fit1Dfor[["se.fit"]]
 
 ##The "predict.Mort1Dsmooth" function can be used to obtain predictions of the smoothed mortality curve over very narrow age intervals (i.e. narrower than the original 1-year intervals), 
-#in order to come closer to the concept of force of mortality (or instantaneous death rate). In our case, each 1-year interval is subdivided into 1,000 equal parts, then:
+ #in order to come closer to the concept of force of mortality (or instantaneous death rate). In our case, each 1-year interval is subdivided into 1,000 equal parts, then:
 
 delta <- 0.001
 age.narrow <- seq(from = min(age), to = max(age), by = delta)
